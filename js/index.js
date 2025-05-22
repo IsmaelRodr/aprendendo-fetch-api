be84b7f5419da9ffcab4bdba4482ab4f5f89e4e3
Ismael Rodrigues <Ismaelneto2003@gmail.com>
2025-05-22 10:06:03 -0300

    aprendendo Fetch API

---
+++ js/index.js
@@ -0,0 +1,101 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
+
+// Get Id From URL
+const urlSearchParams = new URLSearchParams(window.location.search)
+const postId = urlSearchParams.get("id")
+
+
+// Get All Posts
+async function getAllPosts() {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log(data)
+
+    loadingElement.classList.add("hide")
+
+    data.map((post) => {
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const body = document.createElement("p");
+        const link = document.createElement("a");
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "Ler";
+        link.setAttribute("href", `/post.html?id=${post.id}`);
+
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+
+        postsContainer.appendChild(div);
+    })
+}
+
+// Get individual Post
+async function getPost(id){
+    
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`)
+    ]);
+
+    const dataPost = await responsePost.json();
+
+    const dataComments = await responseComments.json();
+
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
+
+    console.log(dataComments)
+
+    dataComments.map((comment) => {
+
+
+    })
+
+}
+
+function createComment(comment){
+
+    const div = document.createElement("div");
+    const email = document.createElement("h3");
+    const commentBody = document.createElement("p");
+
+    email.innerText = comment.email;
+    commentBody.innerText = comment.body;
+
+    div.appendChild(email);
+    div.appendChild(commentBody);
+    
+    commentsContainer.appendChild(div);
+}
+
+if(!postId){
+
+    getAllPosts();
+
+} else{
+
+    getPost(postId);
+}
+
+
